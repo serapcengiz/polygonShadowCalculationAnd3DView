035213a0286b4150bf8c5c6b5f9788a67926f486
serapcengiz <73667009+serapcengiz@users.noreply.github.com>
2023-09-04 10:25:36 +0300

    update2

--- stajProject.py
+++ stajProject.py
@@ -6,7 +6,6 @@ import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
-# Güncellemeleri işleyen fonksiyon
 def update_plot(*args):
     ax.clear()
 
@@ -19,9 +18,9 @@ def update_plot(*args):
     sun_vector_y = math.sin(sun_azimuth) * math.cos(sun_elevation)
     sun_vector_z = math.sin(sun_elevation)
 
-    # Gölge uzunluğunu hesapla
-    shadow_length = object_height * math.tan(math.radians(90 - elevation_scale.get()))
 
+    shadow_length = object_height * math.tan(math.radians(90-elevation_scale.get()))
+    print(shadow_length)
     # Cismin gölge noktalarını hesapla
     shadow_vertices = []
     for vertex in polygon_vertices:
@@ -29,35 +28,31 @@ def update_plot(*args):
         shadow_y = vertex[1] - object_height - shadow_length / sun_vector_z * sun_vector_y
         shadow_z = 0  # Gölgenin z koordinatını sıfır olarak ayarla
         shadow_vertices.append([shadow_x, shadow_y, shadow_z])
-
+    print(shadow_vertices)
     # Gölge noktalarını ve poligon noktalarını çizgiyle birleştir
     lines = []
-    surfaces = []
+    surfaces = []  # Yüzeylerin köşe noktalarını saklamak için
     for i in range(len(shadow_vertices)):
         line = np.array([[shadow_vertices[i][0], shadow_vertices[i][1], shadow_vertices[i][2]],
                          [polygon_vertices[i][0], polygon_vertices[i][1], polygon_vertices[i][2]]])
         lines.append(line)
         surfaces.append([shadow_vertices[i], polygon_vertices[i], polygon_vertices[(i + 1) % len(polygon_vertices)],
                          shadow_vertices[(i + 1) % len(shadow_vertices)]])
-
     # Gölge noktaları arasındaki çizgileri çiz
     for i in range(len(shadow_vertices) - 1):
         line = np.array([[shadow_vertices[i][0], shadow_vertices[i][1], shadow_vertices[i][2]],
                          [shadow_vertices[i + 1][0], shadow_vertices[i + 1][1], shadow_vertices[i + 1][2]]])
         lines.append(line)
         surfaces.append([shadow_vertices[i], shadow_vertices[i + 1], polygon_vertices[i + 1], polygon_vertices[i]])
-
     line_collection = Line3DCollection(lines, colors='gray')
     ax.add_collection3d(line_collection)
-
     # Yüzeyleri gri renkle çiz
     surfaces_collection = Poly3DCollection(surfaces, facecolors='gray', edgecolors='gray', alpha=0.5)
     ax.add_collection3d(surfaces_collection)
-
+    # ... (Çizim işlemlerini burada güncelle) ...
     # Poligonu oluştur
-    poly3d = [[vertice for vertice in face] for face in faces]
+    poly3d = [[vertice for vertice in face] for face in faces]  # Köşe noktalarını düzgünce kullan
     ax.add_collection3d(Poly3DCollection(poly3d, facecolors='red', linewidths=1, edgecolors='red', alpha=0.5))
-
     # Gölge noktalarını çiz
     shadow_vertices = np.array(shadow_vertices)
     ax.scatter(shadow_vertices[:, 0], shadow_vertices[:, 1], shadow_vertices[:, 2], color='gray', marker='o')
@@ -66,11 +61,10 @@ def update_plot(*args):
     ax.set_zlim(0, 50)
     canvas.draw()
 
-# Kullanıcıdan cisim yüksekliğini al
 object_height = float(input("Enter object height: "))
 object_height = int(object_height)
 
-# Cismin köşe noktalarını tanımla
+
 polygon_vertices = np.array([
     [0, 0, 0],
     [0, 30, 0],
@@ -78,10 +72,9 @@ polygon_vertices = np.array([
     [10, 15, 0],
     [25, 15, 0],
     [25, 0, 0],
-    [0, 0, 0]
+    [0,0,0]
 ])
 top_vertices = polygon_vertices + [0, 0, float(object_height)]
-
 # Poligonun yüzlerini tanımla
 faces = [
         [polygon_vertices[0], polygon_vertices[1], top_vertices[1], top_vertices[0]],  # Alt yüz 1
@@ -92,6 +85,7 @@ faces = [
         [polygon_vertices[0], polygon_vertices[1], polygon_vertices[2], polygon_vertices[3], polygon_vertices[4], polygon_vertices[5]], # Taban yüz
         [top_vertices[0], top_vertices[1], top_vertices[2], top_vertices[3], top_vertices[4], top_vertices[5]], # Üst yüz
 ]
+
 
 # Tkinter penceresini oluştur
 root = tk.Tk()
